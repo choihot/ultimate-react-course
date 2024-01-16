d567fb90924a78bc27afe82ec269fa776fc60496
choihot <319054668@qq.com>
2024-01-16 23:13:31 +0800

    feat: finish delete item and check item function

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -1,8 +1,4 @@
 import { useState } from "react";
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
 
 function Logo() {
   return <h1>✈️ Far Away 📝</h1>
@@ -53,24 +49,24 @@ function Form({ items, setItems }) {
   )
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem, onCheckItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item {...item} key={item.id} />
+          <Item {...item} onDeleteItem={onDeleteItem} onCheckItem={onCheckItem} key={item.id} />
         ))}
       </ul>
     </div>
   )
 }
 
-function Item({ description, quantity, packed }) {
+function Item({ description, quantity, packed, id, onDeleteItem, onCheckItem }) {
   return (
     <li>
-      <input type="checkbox" />
+      <input type="checkbox" value={packed} onChange={() => onCheckItem(id)} />
       <span style={packed ? { textDecoration: 'line-through' } : {}}>{quantity} {description}</span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(id)}>❌</button>
     </li>
   )
 }
@@ -86,12 +82,30 @@ function Stats() {
 }
 
 export default function App() {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
+
+  function handleDeleteItem(id) {
+    setItems(items => items.filter(item => item.id !== id));
+  }
+
+  function handleCheckItem(id) {
+    setItems(items => items.map(item => {
+      if (item.id === id) {
+        return { ...item, packed: !item.packed };
+      }
+      return item;
+    }));
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form items={items} setItems={setItems} />
-      <PackingList items={items} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onCheckItem={handleCheckItem}
+      />
       <Stats />
     </div>
   )
